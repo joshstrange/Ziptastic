99abc4fa18e3112d003f6677b0197ab3fc6d7b4a
Josh Strange <josh@joshstrange.com>
2012-04-10 16:36:57 -0400

    Added Github Link

--- index.php
+++ index.php
@@ -88,6 +88,7 @@ $app->get('/', function () {
             </style>
         </head>
         <body>
+        <a href="https://github.com/joshstrange/Ziptastic"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://a248.e.akamai.net/assets.github.com/img/71eeaab9d563c2b3c590319b398dd35683265e85/687474703a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f677261795f3664366436642e706e67" alt="Fork me on GitHub"></a>
             <header>
                 <a href="http://www.slimframework.com">Built With <img src="logo.png" alt="Slim"/></a>
             </header>
